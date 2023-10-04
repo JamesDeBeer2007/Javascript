b8ab5ab0305340fcdc9df4c5620b15fa41a20cc1
James <143798522+JamesDeBeer2007@users.noreply.github.com>
2023-10-04 10:50:28 +0200

    6.1
    6.1

---
+++ khgdajhkknjvgfxdzdgfgn/app.js
@@ -0,0 +1,12 @@
+function changeText()
+{
+    document.getElementById("titles").innerHTML = "SHWVSOD1A";
+}
+
+function changeColor ()
+{
+    document.getElementById("titles").style.color = "blue";
+    console.log("de functie werkt");
+}
+
+changeColor();
