f6fa64e3696a7fd24316c0a230fb9f0a2b6ee030
James <143798522+JamesDeBeer2007@users.noreply.github.com>
2023-12-11 15:37:08 +0100

    1 update

--- traffic_light_1/app.js
+++ traffic_light_1/app.js
@@ -1,9 +1,9 @@
 function changeTrafficLight(){
     trafficLight = document.getElementById("traffic-light")
-    if(document.getElementById("traffic-light").src = "trafficLightRed.png"){
+    if(document.getElementById("traffic-light").src.match("trafficLightRed.png")){
         document.getElementById("traffic-light").src = "trafficLightGreen.png";
     } 
-    else if(document.getElementById("traffic-light").src = "trafficLightGreen.png"){
+    else if(document.getElementById("traffic-light").src.match("trafficLightGreen.png")){
         document.getElementById("traffic-light").src = "trafficLightYellow.png";
     } 
     else {
